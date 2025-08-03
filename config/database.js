655ef3fc66bb0be02bbe112b138a6913076417a0
hanysaadeldeen <hanymohamedsaadeldean@gmail.com>
2025-08-03 23:43:55 +0300

    handle error outside express

--- config/database.js
+++ config/database.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 const DB = process.env.dataBase_Url;
 const dbConnection = () => {
-  mongoose
-    .connect(DB)
-    .then((con) => {
-      // console.log("Connected to MongoDB", con.connection.host);
-    })
-    .catch((error) => {
-      console.log("Error connecting to MongoDB", error);
-      process.exit();
-    });
+  mongoose.connect(DB).then((con) => {
+    // console.log("Connected to MongoDB", con.connection.host);
+  });
+  // .catch((error) => {
+  //   console.log("Error connecting to MongoDB", error);
+  //   process.exit();
+  // });
 };
 
 module.exports = dbConnection;
--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const morgan = require("morgan");
-// const dotenv = require("dotenv");
-// dotenv.config({ path: ".env" });
-
-// const dbConnection = require("./config/database");
-// const projectRoute = require("./routes/ProjectRoute");
-// const categoryRoute = require("./routes/CagetoryRoute");
-// // Connect with db
-// dbConnection();
-// // express app
-// const app = express();
-
-// // middleWares
-// app.use(express.json());
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
-//   console.log(`"Server mode ${process.env.NODE_ENV}`);
-// }
-
-// // routes
-// app.use("/api/v1/projects", projectRoute);
-// app.use("/api/v1/categories", categoryRoute);
-
-// // listen to your project
-// const PORT = process.env.PORT;
-// app.listen(PORT, () => {
-//   console.log(`"Server is running on port ${PORT}`);
-// });
-
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 
@@ -47,6 +17,16 @@ dbConnection();
 
 // listen to your project
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   // console.log(`Server is running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  // error outside express
+  console.error(`"Unhandled Rejection at: ${err.name} || ${err.message}"`);
+  // close server before exit
+  server.close(() => {
+    console.log("shutting down...");
+    process.exit(1);
+  });
 });
