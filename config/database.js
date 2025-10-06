21284da9408656c51cd1c321a7134c252149253b
hanysaadeldeen <hanymohamedsaadeldean@gmail.com>
2025-10-06 22:24:27 +0300

    test deployment2

--- config/database.js
+++ config/database.js
@@ -1,13 +1,25 @@
 const mongoose = require("mongoose");
 const DB = process.env.DB_Url;
-const dbConnection = () => {
-  mongoose.connect(DB).then((con) => {
-    // console.log("Connected to MongoDB", con.connection.host);
-  });
-  // .catch((error) => {
-  //   console.log("Error connecting to MongoDB", error);
-  //   process.exit();
-  // });
+// const dbConnection = () => {
+//   mongoose.connect(process.env.MONGO_URI).then((con) => {
+//     // console.log("Connected to MongoDB", con.connection.host);
+//   });
+//   // .catch((error) => {
+//   //   console.log("Error connecting to MongoDB", error);
+//   //   process.exit();
+//   // });
+// };
+
+// module.exports = dbConnection;
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = dbConnection;
